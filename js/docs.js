2875647b38786cca16c903662fa9de2a674747af
vlad <vladislavbokovoy@gmail.com>
2025-08-15 20:03:44 -0700

    docs 90%

---
+++ js/docs.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const nav = document.querySelector('.DOCS-tabs');
+  const tabslist = document.querySelectorAll('.DOCS-tabs button');
+ 
+  tabslist.forEach(tab => {
+    tab.addEventListener('click', (event) => {
+        tabslist.forEach(btn => btn.classList.remove('active'));
+        tabslist.forEach(btn => { document.getElementById(btn.id + '-Content').style.display = "None" });
+        tab.classList.add('active');
+        document.getElementById(tab.id + '-Content').style.display = "Block"
+    });
+  });
+  
+});
